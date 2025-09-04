ac4b1518339d12875db3224c3431336b6d0758c7
Gravy Boat <gravy@thatgravyboat.tech>
2025-09-04 06:08:21 -0230

    parse variants

--- core/src/main/java/tech/thatgravyboat/repolib/core/data/Item.java
+++ core/src/main/java/tech/thatgravyboat/repolib/core/data/Item.java
@@ -21,33 +21,34 @@ public record Item(
         RepoLocation repoId,
         JsonElement name,
         List<JsonElement> lore,
+        Map<String, Item> variants,
         @Nullable Skin skin,
-        @Nullable String variantSelector,
-        @Nullable Map<String, Item> variants
+        @Nullable String variantSelector
 ) {
 
-    public static Codec<Item> CODEC = RecordCodecBuilder.create(it ->
+    public static Codec<Item> CODEC = Codec.recursive("Item", self -> RecordCodecBuilder.create(it ->
             it.group(
                     RepoCodec.RESOURCE_LOCATION.fieldOf("model").forGetter(Item::model),
                     RepoCodec.STRING.fieldOf("skyblock_id").forGetter(Item::skyblockId),
                     RepoCodec.REPO_LOCATION.fieldOf("repo_id").forGetter(Item::repoId),
                     RepoCodec.JSON.fieldOf("name").forGetter(Item::name),
                     RepoCodec.JSON.listOf().fieldOf("lore").forGetter(Item::lore),
+                    Codec.unboundedMap(RepoCodec.STRING, self).optionalFieldOf("variants", Map.of()).forGetter(Item::variants),
                     RepoCodec.SKIN.optionalFieldOf("skin").forGetter(RepoUtils.optionalGetter(Item::skin)),
                     RepoCodec.STRING.optionalFieldOf("variant_selector").forGetter(RepoUtils.optionalGetter(Item::variantSelector))
             ).apply(
                     it,
-                    (model, skyblockId, repoId, name, lore, skin, variantSelector) -> new Item(
+                    (model, skyblockId, repoId, name, lore, variants, skin, selector) -> new Item(
                             model,
                             skyblockId,
                             repoId,
                             name,
                             lore,
+                            variants,
                             skin.orElse(null),
-                            variantSelector.orElse(null),
-                            Map.of()
+                            selector.orElse(null)
                     )
             )
-    );
+    ));
 
 }
